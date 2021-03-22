94a3bd8bae405abe6ddd4ac23c87bded91b83c5d
gabriellacampos <70173839+gabriellacampos@users.noreply.github.com>
2021-03-22 12:40:00 -0400

    Add files via upload

--- videoGame/script.js
+++ videoGame/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   var mugen = new MUGEN();
-  mugen.initialize({titleScreenImageURL: "./img/title-screen.png", 
+  mugen.initialize({titleScreenImageURL: "./img/title-screen.gif", 
   	actionRadius: 100, timeBetweenNPCs: 3200, 
   	npcCollisionRadius: 50, actionDuration:600, 
-  	jumpHeight: 130});
+  	jumpHeight: 230, typesOfNPC: 2});
 });
